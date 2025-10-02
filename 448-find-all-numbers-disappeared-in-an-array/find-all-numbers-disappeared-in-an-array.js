01a7dab2e536fe5d11ae7bfc23301c96dc5da119
Mohanraja Manickam <150321632+MohanrajaM@users.noreply.github.com>
2025-10-02 15:53:01 +0530

    Time: 24 ms (28.39%) | Memory: 79.1 MB (11.01%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+
+let set = new Set(nums);
+    let output = [];
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (!set.has(i)) {
+            output.push(i);
+        }
+    }
+    return output;
+    
+};
