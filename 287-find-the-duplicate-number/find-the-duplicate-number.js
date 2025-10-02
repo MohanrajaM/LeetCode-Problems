b0430a74db1f3218d857275378bf8897947145d5
Mohanraja Manickam <150321632+MohanrajaM@users.noreply.github.com>
2025-10-02 12:55:11 +0530

    Time: 16 ms (47.29%) | Memory: 77.5 MB (14.69%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+ const seen = new Set();
+    for (let num of nums) {
+        if (seen.has(num)) return num;
+        seen.add(num);
+    }
+    return -1; // no duplicate
+};
