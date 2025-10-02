4c68bc62a3ba24654317f2432cfb5936b45b42ae
Mohanraja Manickam <150321632+MohanrajaM@users.noreply.github.com>
2025-10-02 20:03:29 +0530

    Time: 17 ms (65.03%) | Memory: 61.1 MB (66.37%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.js
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.js
@@ -6,14 +6,11 @@
 var kthSmallest = function(matrix, k) {
   var margedArray=[]
     for ( var i of matrix){
-
         for( var j of i){
             margedArray.push(j)
-        }
-        
+        }    
     }
     margedArray = margedArray.sort((a,b)=>a-b);
     var output = margedArray[k-1];
-
     return output
 };
